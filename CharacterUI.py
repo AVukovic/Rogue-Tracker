05924fb2181ff745e53b59f194fc55da02f2b125
AVukovic <alex.vukovic@mail.utoronto.ca>
2015-05-04 10:25:20 -0400

    Finished "delete" functionality

--- CharacterUI.py
+++ CharacterUI.py
@@ -49,54 +49,37 @@ def make_credits(*args):
     messagebox.showinfo(message=
     'Rogue Tracker (v 0.01)\nDeveloped by Alexander Vukovic', title = "Credits")
     
-"""def get_name(thing):
+def get_name(thing):
     index = thing.index(":")
     return thing[0:index]
-
-#def make_new_stat(name,num):
-    return (name + ": " + num + "/" + num)
-
-#def commit_stat(stat_num):
-    return (str(stat_num).isdigit())
-
-#def stat_button_enter(name,num):
-    if not(name == "") and not(num == "") and (commit_stat(num)):
-        stats.append(make_new_stat(name,num))
-        box_list.populate(stats)
-        top.destory()
-    else:
-        pass
-    
-
-#def add_stat(*args):
-    top = Toplevel()
-    top.title("Add Entry")
-    frame = ttk.Frame(top, padding = "2 2 3 3")
-    name_label = ttk.Label(top, text = "Enter name of stat:")
-    name = StringVar()
-    name_entry = ttk.Entry(top, width = 10, textvariable = name)
-    max_stat_label = ttk.Label(top,text = "Enter max # of stat(# only!):")
-    max_stat = StringVar()
-    max_stat_entry = ttk.Entry(top, width = 5, textvariable = max_stat)
-    enter_stat_button = ttk.Button(top, text = "Save Stat", 
-                                   command = stat_button_enter(name, max_stat))
-    exit_button = ttk.Button(top,text = "Cancel", command = top.destroy())
-    name_label.pack() """
-    
     
 
 def make_stats(*args):
     populate(stats)
-    current_list = "stats"
-    
+    global is_stats
+    global is_items 
+    global is_skills
+    is_stats = True
+    is_items = False
+    is_skills = False
 
 def make_inventory(*args):
     populate(items)
-    current_list = "items"
+    global is_stats
+    global is_items 
+    global is_skills    
+    is_stats = False
+    is_items = True
+    is_skills = False
     
 def make_skills(*args):
     populate(skills)
-    current_list = "skills"
+    global is_stats
+    global is_items 
+    global is_skills    
+    is_stats = False
+    is_items = False
+    is_skills = True
 
 def populate(lst):
     box_list.delete(0,END)
@@ -104,22 +87,41 @@ def populate(lst):
         box_list.insert(END,x)
 
 def add():
-    if current_list == "stats":
+    if is_stats:
         stats.append("Stat: 0/0")
         make_stats()
-    elif current_list == "items":
+    elif is_items:
         items.append("Item: x1")
         make_inventory()
-    elif current_list == "skills":
+    elif is_skills:
         skills.append("Skill: Castable")
         make_skills()
-        
+
+def stat_edit():
+    top = Toplevel()
+    top.title("Edit Value")
+    
+    msg = Message(top, text="Edit your stat here!")
+    msg.pack()
+    
+    button = Button(top, text="Dismiss", command=top.destroy)
+    button.pack()    
 
 def edit(*args):
-    pass
+    stat_edit()
 
-def delete(*args):
-    pass
+def delete():
+    idx = box_list.get(box_list.curselection())
+    if is_stats:
+        stats.remove(idx)
+        make_stats()
+    elif is_items:
+        items.remove(idx)
+        make_inventory()
+    elif is_skills:
+        skills.remove(idx)
+        make_skills()
+        
 
 """Definitions of all widgets starts here:
 ____________________________________________________________________________"""
@@ -127,12 +129,14 @@ ____________________________________________________________________________"""
 stats = ["HP: 20/20"]
 skills = ["Haste: Castable"]
 items = ["Gold: x100"]
-current_list = "stats"
+is_stats = True
+is_items = False
+is_skills = False
 #root frame
 root = Tk()
 root.title("Rogue Tracker (v0.01)")
 root.resizable(0,0)
-main_frame = ttk.Frame(root, padding = "3 3 48 12")
+main_frame = ttk.Frame(root, padding = "12 12 12 24")
 main_frame.grid(column = 0, row = 0)
 
 #Name_Frame Definitions
@@ -140,7 +144,7 @@ name_frame = ttk.Frame(main_frame, padding = "1 1 4 4")
 player_name = StringVar()#string being displayed by Name_Label
 player_name.set("John Doe") #placeholder string
 name_getter = StringVar() #string fetched from NAMEBUTTON
-entry_name_label = ttk.Label(name_frame, text = "Enter your new name here!")
+entry_name_label = ttk.Label(name_frame, text = "Enter name here:")
 name_button = ttk.Button(name_frame, text = "Edit Name", command = rename)
 entry_box = ttk.Entry(name_frame, width = 14, textvariable = name_getter)
 
@@ -166,10 +170,9 @@ empty_box = ttk.Frame(empty_frame, relief = "sunken")
 stuff_frame = ttk.Frame(main_frame, padding = "3 3 12 12")
 list_box = ttk.Frame(stuff_frame, padding = "2 2 8 8")
 box_scroll = Scrollbar(list_box)
-box_list = Listbox(list_box,height = 3,yscrollcommand=box_scroll.set)
+box_list = Listbox(list_box,height = 3,yscrollcommand= box_scroll.set)
 populate(stats)
 box_scroll.config(command=box_list.yview)
-box_scroll = Scrollbar(list_box)
 edit_box = ttk.Frame(list_box, padding = "6 6 4 4")
 add_value = ttk.Button(edit_box, text = "Add Value", command = add)
 edit_value = ttk.Button(edit_box, text = "Edit Value", command = edit)
@@ -211,7 +214,7 @@ stuff_frame.grid(column = 1, row = 4)
 list_box.grid(column = 0, row = 1)
 box_scroll.grid(column = 1, row = 1)
 box_list.grid(column = 0, row = 1)
-edit_box.grid(column = 1, row = 1)
+edit_box.grid(column = 2, row = 1)
 add_value.grid(column = 0, row = 0)
 edit_value.grid(column = 0, row = 1)
 delete_value.grid(column = 0, row = 2)
